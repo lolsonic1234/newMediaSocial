7e8148ad6294a81d3386e8191280ff38ecf80dcb
lolsonic1234 <anthonydaluz@hotmail.com>
2016-02-17 11:14:59 -0500

    Update logIn.js

--- logIn.js
+++ logIn.js
@@ -3,6 +3,30 @@ Parse.initialize("SLgQi40ZWuXta0IwBzaKNxSVE2vUQveZRQHgREaw", "lVYH1g2UpO9PokeMDr
 var p1332ddfd = "";
 var kgmfFFtY = "";
 
+var year = new Date().getFullYear();
+var day = new Date().getDate();
+var hours = new Date().getHours();
+var minutes = new Date().getMinutes();
+var seconds = new Date().getSeconds();
+
+
+setInterval(currentTime(), 1000)
+
+
+function currentTime(){
+    if(hours < 10){
+        hours = "0" + new Date().getHours();
+    }
+    
+    if(minutes < 10){
+        minutes = "0" + new Date().getMinutes();
+    }
+    
+    if(seconds < 10){
+        seconds = "0" + new Date().getSeconds();
+    }
+}
+
 
 var updater = setInterval(updateStatuses, 2000);
 clearInterval(updater);
@@ -35,8 +59,7 @@ function clickThis(){
         $('.logedInDiv').css("display", "block");
         $('#userNaMe').html(kgmfFFtY)
         alert('welcome');
-        alert('Do me a favor. When posting a status, wait for 4 seconds and it will appear. If you spam it, no one will be able to use the website. By the way I have the power to see who spammed it. I hooked my website with the power of seeing who spammed my website.');
-        updater = setInterval(updateStatuses, 4000);
+        updater = setInterval(updateStatuses, 2500);
     }else {
         alert("go away.")
     }
@@ -59,16 +82,18 @@ function updateStatuses(){
     var GameScore = Parse.Object.extend("statusEs");
     var query = new Parse.Query(GameScore);
     
-    query.notEqualTo("kgmfFFtY", "17777777774");
+    query.descending("kgmfFFtY");
+    
+    query.notEqualTo("kgmfFFtY", "1777777777877694");
     query.find({
       success: function(results) {
           $(".middleBar").empty();
         // Do something with the returned Parse.Object values
-        for (var i = 0; i < results.length; i++) {
-          var object = results[i];
-          
-          $(".middleBar").append("<div class='temp' style='background-color: darkgray; border-radius: 10px;'><h1 class='poster' style='font-size: 25px; text-align: center;'>" + object.get("kgmfFFtY") +"</h1><h4 class='posted' style='margin-left: 5px;'>" + object.get("status") + "</h4></div>")
-        }
+            for (var i = 0; i < results.length; i++) {
+              var object = results[i];
+              alert(object.get("kgmfFFtY"))
+              $(".middleBar").append("<div class='temp' style='background-color: darkgray; border-radius: 10px;'><h1 class='poster' style='font-size: 25px; text-align: center;'>" + object.get("kgmfFFtY") +"</h1><h4 class='posted' style='margin-left: 5px;'>" + object.get("status") + "</h4></div>");
+            }
       },
       error: function(error) {
         alert("Error: " + error.code + " " + error.message);
